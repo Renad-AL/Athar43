1e0e1edce303be256fec4fa6f4862d7c8ae44dd5
Renad <uniirena@gmail.com>
2024-12-16 17:10:45 +0300

    Initial Commit

---
+++ Athar43/Athar43App.swift
@@ -0,0 +1,17 @@
+//
+//  Athar43App.swift
+//  Athar43
+//
+//  Created by Renad Alyousef on 15/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Athar43App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Athar43Tests/Athar43Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Athar43Tests.swift
+//  Athar43Tests
+//
+//  Created by Renad Alyousef on 15/06/1446 AH.
+//
+
+import Testing
+@testable import Athar43
+
+struct Athar43Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
